4d0885397d16e669d22f375fda5a67d0a8d802d2
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-11-21 19:29:05 +0900

    feat: build project starndarts

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,12 @@
+export enum Message {
+	SOMETHING_WENT_WRONG = 'Something went wrong',
+	NO_DATA_FOUND = 'No data is found',
+	CREATE_FAILED = 'Create is failed',
+	UPDATE_FAILED = 'Update is failed',
+	NO_MEMBER_NICK = 'NO member with that member nick!',
+	BLOCKED_USER = ' You have been blocked, contact restaurant',
+	USED_NICK_PHONE = 'You are inserting already used nick or phone!',
+	TOKEN_CREATION_FAILED = ' Token creation error',
+	WRONG_PASSWORD = 'Wrong password, please try again!',
+	NOT_AUTHENTIFICATED = 'You are not authentificated , Please , login first !',
+}
