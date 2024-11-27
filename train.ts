23fa9e467d48f798bde9bcdac9852e43e5bef9ec
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-11-28 02:45:55 +0900

    feat: adding train.ts file , nodemon and TASK - ZM

---
+++ train.ts
@@ -0,0 +1,9 @@
+function reverseInteger(num: number): number {
+	const reversedString = Math.abs(num).toString().split('').reverse().join('');
+
+	const reversedNumber = parseInt(reversedString, 10) * Math.sign(num);
+	return reversedNumber;
+}
+
+console.log(reverseInteger(123456789));
+console.log(reverseInteger(-123456789));
