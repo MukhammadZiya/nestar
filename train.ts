6c96a45b24b4a3b4123984c2a44c0f4c29a8dfd3
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-12-02 02:19:16 +0900

    feat: TASK ZN

--- train.ts
+++ train.ts
@@ -1,11 +1,23 @@
+// function reverseInteger(num: number): number {
+// 	const reversedString = Math.abs(num).toString().split('').reverse().join('');
 
+// 	const reversedNumber = parseInt(reversedString, 10) * Math.sign(num);
+// 	return reversedNumber;
+// }
 
-function reverseInteger(num: number): number {
-	const reversedString = Math.abs(num).toString().split('').reverse().join('');
+// console.log(reverseInteger(123456789));
+// console.log(reverseInteger(-123456789));
 
-	const reversedNumber = parseInt(reversedString, 10) * Math.sign(num);
-	return reversedNumber;
+// TASK ZN:
+
+function rotateArray(arr: number[], n: number): number[] {
+	const length = arr.length;
+	const index = n % length;
+
+	const firstPart = arr.slice(index);
+	const secondPart = arr.slice(0, index);
+
+	return [...firstPart, ...secondPart];
 }
 
-console.log(reverseInteger(123456789));
-console.log(reverseInteger(-123456789));
+console.log(rotateArray([1, 2, 3, 4, 5, 6], 3));
