5ae416369df6f926e3c5ab53df3aba907d5c939c
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-12-05 19:55:20 +0900

    feat: mit task

--- train.ts
+++ train.ts
@@ -10,14 +10,35 @@
 
 // TASK ZN:
 
-function rotateArray(arr: number[], n: number): number[] {
-	const length = arr.length;
-	const index = n % length;
+// function rotateArray(arr: number[], n: number): number[] {
+// 	const length = arr.length;
+// 	const index = n % length;
 
-	const firstPart = arr.slice(index);
-	const secondPart = arr.slice(0, index);
+// 	const firstPart = arr.slice(index);
+// 	const secondPart = arr.slice(0, index);
 
-	return [...firstPart, ...secondPart];
+// 	return [...firstPart, ...secondPart];
+// }
+
+// console.log(rotateArray([1, 2, 3, 4, 5, 6], 3));
+
+function areArraysEqual(arr1: number[], arr2: number[]): boolean {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	arr1.sort();
+	arr2.sort();
+
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
-console.log(rotateArray([1, 2, 3, 4, 5, 6], 3));
+console.log(areArraysEqual([1, 2, 3], [3, 1, 2]));
+console.log(areArraysEqual([1, 2, 3], [3, 1, 2, 1]));
+console.log(areArraysEqual([1, 2, 3], [4, 1, 2]));
